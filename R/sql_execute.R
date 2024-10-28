ca28a1e03769eafe9422b341b32db49ce5bbf355
Pieta <pieta.schofield@me.com>
2024-10-28 14:33:20 +0000

    adding the 3 stage sql function sql_execute to circumvent the limit on query size

---
+++ R/sql_execute.R
@@ -0,0 +1,25 @@
+#' execute and sql file in a duckdb
+#'
+#' @export
+sql_execute <- function(dbf,sql_make,sql_query,sql_drop,db=F){
+  if(db){
+    dbf <- adb
+    sql_make <- make_sql
+    sql_make
+    sql_query <- query_sql
+    sql_query
+    sql_drop <- drop_sql
+    sql_drop
+  }
+  # Connect to your DuckDB database
+  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir = dbf, write=T)
+
+  # Register and execute the external SQL script
+  nret <- DBI::dbExecute(con, sql_make)
+
+# Fetch results if the query is a SELECT
+  res <- DBI::dbGetQuery(con, sql_query) %>% as_tibble()
+  nret <- DBI::dbExecute(con, sql_drop)
+  duckdb::dbDisconnect(con,shutdown=T)
+  return(res)
+}
